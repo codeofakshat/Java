7b889e6a31801f1b1cb3798db0c5338f18caeaaf
zex <codingforakshat@gmail.com>
2023-09-16 21:36:00 +0530

    Inputs1.java

---
+++ src/akshat/Inputs1.java
@@ -0,0 +1,20 @@
+package akshat;
+
+import java.util.Scanner;
+
+public class Inputs1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter some input: ");
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll no is : " + rollno);
+
+        System.out.println("Enter some string");
+        String name = input.nextLine();
+        System.out.println(name);
+
+        System.out.println("Enter some float value");
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
