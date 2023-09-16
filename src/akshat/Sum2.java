514d908a6d05f0f6d121db3024843552cf7997bc
zex <codingforakshat@gmail.com>
2023-09-16 22:27:26 +0530

    Sum2.java

---
+++ src/akshat/Sum2.java
@@ -0,0 +1,17 @@
+package akshat;
+
+import java.util.Scanner;
+
+public class Sum2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1= input.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println(" Sum = " + sum);
+    }
+}
