4522a40512696d6cafb2d47e3508d9b302dd19da
zex <codingforakshat@gmail.com>
2023-09-16 21:15:57 +0530

    Primitives.java

--- src/akshat/Main.java
+++ src/akshat/Main.java
@@ -1,5 +1,7 @@
 package akshat;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ src/akshat/Primitives.java
@@ -0,0 +1,14 @@
+package akshat;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 435345.64464;
+        long largeIntegervalues = 43534348639400L;
+        boolean check = false;
+
+
+    }
+}
