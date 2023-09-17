3661a7c28ef7e0c19f1cb63c3e36bad778ced3a6
zex <codingforakshat@gmail.com>
2023-09-17 19:26:34 +0530

    basic for loop statement

---
+++ src/ConditionalsAndLoops/forloop2.java
@@ -0,0 +1,18 @@
+package ConditionalsAndLoops;
+
+public class forloop2 {
+    public static void main(String[] args) {
+        /*
+    Syntax of for loop:
+        for (initialization; condition; increment/decrement){
+            body
+            }
+    */
+
+        //Question - Print numbers 1 to 10
+        for (int count=1; count<=10; count++){
+            System.out.println(count);
+        }
+    }
+
+}
