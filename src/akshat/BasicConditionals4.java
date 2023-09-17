2cfe5bac0a41d598fe6415b830d238de9fc08e3f
zex <codingforakshat@gmail.com>
2023-09-17 17:49:23 +0530

    BasicConditionals4.java

---
+++ src/akshat/BasicConditionals4.java
@@ -0,0 +1,16 @@
+package akshat;
+
+public class BasicConditionals4 {
+    public static void main(String[] args) {
+        /*int a = 10;
+        if (a == 10) {
+            System.out.println("Hello World");*/
+
+        int count = 1;
+        while (count!=5){
+            System.out.println(count);
+            count++;
+
+        }
+    }
+}
