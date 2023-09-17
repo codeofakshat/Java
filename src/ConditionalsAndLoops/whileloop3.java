a9cd1c0b703500d27b96f93ffef3ded41ad8b4e5
zex <codingforakshat@gmail.com>
2023-09-17 22:37:31 +0530

    basics for loop statements

---
+++ src/ConditionalsAndLoops/whileloop3.java
@@ -0,0 +1,21 @@
+package ConditionalsAndLoops;
+
+public class whileloop3 {
+    public static void main(String[] args) {
+
+        /*
+
+        While loop syntax:
+        while (condition){
+            body
+        }
+
+        */
+        int num=1;
+        while (num<=10){
+            System.out.println(num);
+            num++;
+//            or num += 1;
+        }
+    }
+}
