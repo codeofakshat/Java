5f28e19c7a2675c7beed77c0aa34c755e41cbd25
zex <codingforakshat@gmail.com>
2023-09-17 22:38:44 +0530

    basics for loop statements

---
+++ src/ConditionalsAndLoops/dowhileLoop4.java
@@ -0,0 +1,19 @@
+package ConditionalsAndLoops;
+
+public class dowhileLoop4 {
+    public static void main(String[] args) {
+        /*
+        Syntax for do while loop
+        do {
+            body
+        }while (condition);
+
+        */
+        //difference is that whatever is in do body will execute once no matter what
+
+        int n = 1;
+        do{
+            System.out.println("Hello World");
+        }while(n!=1); //even tho condition contradicts it still prints once
+    }
+}
