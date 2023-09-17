2901f265d96908afe8bb43c6521493cbee1259ed
zex <codingforakshat@gmail.com>
2023-09-17 17:55:16 +0530

    Temperature Conversion

---
+++ src/akshat/TemperatureProgram5.java
@@ -0,0 +1,14 @@
+package akshat;
+
+import java.util.Scanner;
+
+public class TemperatureProgram5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter temperature in Celsius: ");
+        float tempC = in.nextFloat();
+
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println("Temperature in Fahrenheit is: " + tempF);
+    }
+}
