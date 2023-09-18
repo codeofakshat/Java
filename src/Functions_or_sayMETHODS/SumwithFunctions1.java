4e50795f56405779d744eca13ec5826170b2a6e2
zex <codingforakshat@gmail.com>
2023-09-18 22:42:55 +0530

    basic Sum but with method/function Sum()

---
+++ src/Functions_or_sayMETHODS/SumwithFunctions1.java
@@ -0,0 +1,31 @@
+package Functions_or_sayMETHODS;
+
+import java.util.Scanner;
+
+public class SumwithFunctions1 {
+    public static void main(String[] args) {
+        /*
+        basic syntax for function/method:
+
+        Access modifier return_type name() {
+            //body
+            return statement;
+        }
+        */
+
+        //called sum function
+        sum();
+
+
+    }
+    //create sum function
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
