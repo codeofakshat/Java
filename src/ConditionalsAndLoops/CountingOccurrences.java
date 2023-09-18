1347506139361c7c6c08da70049af2b616b15775
zex <codingforakshat@gmail.com>
2023-09-18 19:38:24 +0530

    Counting occurrences of a number

---
+++ src/ConditionalsAndLoops/CountingOccurrences.java
@@ -0,0 +1,23 @@
+package ConditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class CountingOccurrences {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = in.nextInt();
+        System.out.println("Which number do you want to search? ");
+        int j = in.nextInt();
+        int count = 0;
+        while (n > 0){
+            int rem = n % 10;
+
+            if (rem == j){
+                count++;
+            }
+            n=n/10;
+        }
+        System.out.println("The number " + j + " occurred " + count + " times in the number");
+    }
+}
