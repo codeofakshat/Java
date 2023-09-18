1bf398cf9b18a7f9f0fb41756da6f68c652f829b
zex <codingforakshat@gmail.com>
2023-09-18 23:39:59 +0530

    Basic swap two numbers

---
+++ src/Functions_or_sayMETHODS/SwapBasic6.java
@@ -0,0 +1,15 @@
+package Functions_or_sayMETHODS;
+
+public class SwapBasic6 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //Swap numbers code
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+}
