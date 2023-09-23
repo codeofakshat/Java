447d18cf050fe1c4e6b332dc025e94c8ae9d3bbc
zex <codingforakshat@gmail.com>
2023-09-24 01:05:13 +0530

    Basic Arrays

---
+++ src/Arrays/BasicArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class BasicArray {
+    public static void main(String[] args) {
+        //syntax
+        /*
+        datatype[] variable_name = new datatype[size];
+        */
+        //store 5 roll numbers
+        int[] rollnos = new int[5];
+        //or store directly
+        int[] rnos = {23,12,45,32,15};
+    }
+}
