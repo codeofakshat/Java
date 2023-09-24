0f534fac93380c8f5fcd6384c3f48e28d1c07b37
zex <codingforakshat@gmail.com>
2023-09-24 18:15:44 +0530

    Array input and output | another method to print array

---
+++ src/Arrays/Arrayinput2.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arrayinput2 {
+    public static void main(String[] args) {
+        //another method to print
+        Scanner in = new Scanner(System.in);
+        int[] myarray = new int[5];
+        System.out.println("Input 5 elements for array: ");
+        for (int i = 0; i < myarray.length; i++){
+            myarray[i] = in.nextInt();
+        }
+        //here it is:
+        System.out.println(Arrays.toString(myarray));
+    }
+}
