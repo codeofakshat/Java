27ce1c4e7f11ae117259cf4d37b9cf8438af629f
zex <codingforakshat@gmail.com>
2023-09-24 17:51:31 +0530

    Basic Arrays

--- src/Arrays/BasicArray.java
+++ src/Arrays/BasicArray.java
@@ -7,8 +7,17 @@ public class BasicArray {
         datatype[] variable_name = new datatype[size];
         */
         //store 5 roll numbers
-        int[] rollnos = new int[5];
+        /*int[] rollnos = new int[5];
         //or store directly
-        int[] rnos = {23,12,45,32,15};
+        int[] rnos = {23,12,45,32,15};*/
+
+        int[] ros; //declaration of array, ros is getting defined in the stack
+        ros =  new int[5]; //initialisation: actually here object is being created in the memory (heap)
+        System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+
     }
 }
